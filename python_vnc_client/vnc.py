e9e95243fd8b05faafd056835e087cc4b60af02a
Masamitsu MURASE <masamitsu.murase@gmail.com>
2017-06-11 18:08:19 +0900

    Refine variable names.

--- python_vnc_client/vnc.py
+++ python_vnc_client/vnc.py
@@ -66,33 +66,33 @@ class Vnc(object):
     KEY_ALT_RIGHT = 0xFFEA
 
     def __init__(self, url, port=5900, password=None):
-        self.__url = url
-        self.__port = port
-        self.__password = password
-        self.__socket = None
-        self.__image_mutex = threading.Lock()
-        self.__image_cv = threading.Condition(self.__image_mutex)
+        self._url = url
+        self._port = port
+        self._password = password
+        self._socket = None
+        self._image_mutex = threading.Lock()
+        self._image_cv = threading.Condition(self._image_mutex)
 
-        self.__security_type = None
-        self.__server_name = ""
-        self.__receive_thread = None
-        self.__framebuffer_request = False
+        self._security_type = None
+        self._server_name = ""
+        self._receive_thread = None
+        self._framebuffer_request = False
 
         self.__state = "init"
 
     @property
     def security_type(self):
-        return self.__security_type
+        return self._security_type
 
     @property
     def server_name(self):
-        return self.__server_name
+        return self._server_name
 
     def connect(self):
-        if self.__socket is not None:
+        if self._socket is not None:
             raise RuntimeError("socket is already opened.")
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__socket.connect((self.__url, self.__port))
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((self._url, self._port))
 
         version = self.recv(12)
         match_data = re.match(self.VERSION_PATTERN, version)
@@ -106,21 +106,21 @@ class Vnc(object):
             reason = self.receive_string()
             raise RuntimeError("Connection refused: %s" % reason)
         security_types = struct.unpack("%dB" % count, self.recv(count))
-        if self.__password is None:
+        if self._password is None:
             if self.SECURITY_TYPE_NONE not in security_types:
                 raise RuntimeError("Security type 'None' is not supported by the server.")
-            self.__security_type = self.SECURITY_TYPE_NONE
+            self._security_type = self.SECURITY_TYPE_NONE
         else:
             if self.SECURITY_TYPE_VNC not in security_types:
                 raise RuntimeError("Security type VNC is not supported by the server.")
-            self.__security_type = self.SECURITY_TYPE_VNC
-        self.send(struct.pack("B", self.__security_type))
+            self._security_type = self.SECURITY_TYPE_VNC
+        self.send(struct.pack("B", self._security_type))
 
-        if self.__security_type == self.SECURITY_TYPE_NONE:
+        if self._security_type == self.SECURITY_TYPE_NONE:
             pass
-        elif self.__security_type == self.SECURITY_TYPE_VNC:
+        elif self._security_type == self.SECURITY_TYPE_VNC:
             challenge = self.recv(16)
-            response = self.encrypt(self.__password, challenge)
+            response = self.encrypt(self._password, challenge)
             self.send(response)
         else:
             raise NotImplementedError()
@@ -135,17 +135,17 @@ class Vnc(object):
         # ServerInit
         server_init = self.recv(20)
         self.parse_server_init(server_init)
-        self.__server_name = self.receive_string()
+        self._server_name = self.receive_string()
 
         self.__state = "connected"
         self.start_receive_thread()
         self.update_whole_framebuffer(False)
 
     def connect_old(self):
-        if self.__socket is not None:
+        if self._socket is not None:
             raise RuntimeError("socket is already opened.")
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__socket.connect((self.__url, self.__port))
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((self._url, self._port))
 
         version = self.recv(12)
         match_data = re.match(self.VERSION_PATTERN, version)
@@ -155,13 +155,13 @@ class Vnc(object):
 
         # Security Type.
         (security_type, ) = struct.unpack(">L", self.recv(4))
-        self.__security_type = security_type
+        self._security_type = security_type
 
-        if self.__security_type == self.SECURITY_TYPE_NONE:
+        if self._security_type == self.SECURITY_TYPE_NONE:
             pass
-        elif self.__security_type == self.SECURITY_TYPE_VNC:
+        elif self._security_type == self.SECURITY_TYPE_VNC:
             challenge = self.recv(16)
-            response = self.encrypt(self.__password, challenge)
+            response = self.encrypt(self._password, challenge)
             self.send(response)
         else:
             raise NotImplementedError()
@@ -176,22 +176,22 @@ class Vnc(object):
         # ServerInit
         server_init = self.recv(20)
         self.parse_server_init(server_init)
-        self.__server_name = self.receive_string()
+        self._server_name = self.receive_string()
 
         self.__state = "connected"
         self.start_receive_thread()
         self.update_whole_framebuffer(False)
 
     def close(self):
-        if self.__socket:
+        if self._socket:
             self.__state = "closing"
-            self.__socket.shutdown(socket.SHUT_RDWR)
-            if self.__receive_thread:
-                self.__receive_thread.join()
-            self.__socket.close()
-            self.__socket = None
+            self._socket.shutdown(socket.SHUT_RDWR)
+            if self._receive_thread:
+                self._receive_thread.join()
+            self._socket.close()
+            self._socket = None
             self.__state = "closed"
-        self.__receive_thread = None
+        self._receive_thread = None
 
     def encrypt(self, password, challenge):
         if isinstance(password, bytes):
@@ -218,23 +218,23 @@ class Vnc(object):
         return pydes.des(struct.pack("8B", *new_key)).encrypt(text)
 
     def send(self, packet):
-        if self.__socket is None:
+        if self._socket is None:
             raise RuntimeError("socket is closed.")
         totalsent = 0
         length = len(packet)
         while totalsent < length:
-            sent = self.__socket.send(packet[totalsent:])
+            sent = self._socket.send(packet[totalsent:])
             if sent == 0:
                 raise RuntimeError("socket is broken")
             totalsent += sent
 
     def recv(self, length):
-        if self.__socket is None:
+        if self._socket is None:
             raise RuntimeError("socket is closed.")
         totalreceived = 0
         received_data = b""
         while totalreceived < length:
-            chunk = self.__socket.recv(length - totalreceived)
+            chunk = self._socket.recv(length - totalreceived)
             if chunk == b"":
                 if self.__state == "closing":
                     return None
@@ -260,30 +260,30 @@ class Vnc(object):
             self.update_whole_framebuffer(False)
         else:
             self.update_whole_framebuffer(True)
-        with self.__image_mutex:
-            return tuple(map(tuple, self.__image))
+        with self._image_mutex:
+            return tuple(map(tuple, self._image))
 
     def update_whole_framebuffer(self, incremental=True):
         if incremental:
-            packet = struct.pack("BBHHHH", 3, 1, 0, 0, self.__width, self.__height)
+            packet = struct.pack("BBHHHH", 3, 1, 0, 0, self._width, self._height)
         else:
-            packet = struct.pack("BBHHHH", 3, 0, 0, 0, self.__width, self.__height)
+            packet = struct.pack("BBHHHH", 3, 0, 0, 0, self._width, self._height)
         self.reset_update_flag()
         self.send(packet)
         self.wait_for_update()
 
     def reset_update_flag(self):
-        with self.__image_mutex:
-            self.__framebuffer_request = True
+        with self._image_mutex:
+            self._framebuffer_request = True
 
     def wait_for_update(self):
-        with self.__image_mutex:
-            while self.__framebuffer_request == True:
-                self.__image_cv.wait()
+        with self._image_mutex:
+            while self._framebuffer_request == True:
+                self._image_cv.wait()
 
     def start_receive_thread(self):
-        self.__receive_thread = _VncReceiveThread(self)
-        self.__receive_thread.start()
+        self._receive_thread = _VncReceiveThread(self)
+        self._receive_thread.start()
 
     def receive_message(self):
         while True:
@@ -305,9 +305,9 @@ class Vnc(object):
         (num_of_rects, ) = struct.unpack(">H", self.recv(2))
         for _ in range(num_of_rects):
             self.read_rect()
-        with self.__image_mutex:
-            self.__framebuffer_request = False
-            self.__image_cv.notify_all()
+        with self._image_mutex:
+            self._framebuffer_request = False
+            self._image_cv.notify_all()
 
     def process_bell(self):
         pass  # Do nothing
@@ -315,12 +315,12 @@ class Vnc(object):
     def read_rect(self):
         x, y, width, height, enc = struct.unpack(">HHHHl", self.recv(12))
 
-        data = self.recv(width * height * self.__bits_per_pixel // 8)
+        data = self.recv(width * height * self._bits_per_pixel // 8)
 
-        fields = self.__ctypes_fields
-        class Pixel(self.__ctypes_base_class):
+        fields = self._ctypes_fields
+        class Pixel(self._ctypes_base_class):
             _fields_ = fields
-        class Parser(self.__ctypes_base_class):
+        class Parser(self._ctypes_base_class):
             _fields_ = [("pixel", Pixel * width * height)]
         parser = Parser()
         ctypes.memmove(ctypes.addressof(parser), data, len(data))
@@ -329,9 +329,9 @@ class Vnc(object):
         self.update_rect(x, y, width, height, image_matrix)
 
     def update_rect(self, x, y, width, height, image_matrix):
-        with self.__image_mutex:
+        with self._image_mutex:
             for i in range(height):
-                self.__image[y + i][x:(x + width)] = image_matrix[i]
+                self._image[y + i][x:(x + width)] = image_matrix[i]
 
     def receive_string(self):
         (reason_length, ) = struct.unpack(">L", self.recv(4))
@@ -342,34 +342,34 @@ class Vnc(object):
 
     def parse_server_init(self, server_init):
         w, h, bpp, d, bef, tcf, rm, gm, bm, rs, gs, bs = struct.unpack(">HHBBBBHHHBBB3x", server_init)
-        self.__width = w
-        self.__height = h
-        self.__bits_per_pixel = bpp
-        self.__depth = d
-        self.__big_endian_flag = bef
-        self.__true_colour_flag = tcf
-        self.__red_max = rm
-        self.__green_max = gm
-        self.__blue_max = bm
-        self.__red_shift = rs
-        self.__green_shift = gs
-        self.__blue_shift = bs
+        self._width = w
+        self._height = h
+        self._bits_per_pixel = bpp
+        self._depth = d
+        self._big_endian_flag = bef
+        self._true_colour_flag = tcf
+        self._red_max = rm
+        self._green_max = gm
+        self._blue_max = bm
+        self._red_shift = rs
+        self._green_shift = gs
+        self._blue_shift = bs
 
-        self.__image = [[(0, 0, 0) for _ in range(w)] for _ in range(h)]
+        self._image = [[(0, 0, 0) for _ in range(w)] for _ in range(h)]
 
         self.construct_parser()
 
     def construct_parser(self):
-        members = sorted([("red", self.__red_shift, self.__red_max),
-                          ("green", self.__green_shift, self.__green_max),
-                          ("blue", self.__blue_shift, self.__blue_max)],
+        members = sorted([("red", self._red_shift, self._red_max),
+                          ("green", self._green_shift, self._green_max),
+                          ("blue", self._blue_shift, self._blue_max)],
                          key=lambda x: x[1])
 
-        if self.__bits_per_pixel == 8:
+        if self._bits_per_pixel == 8:
             ctype = ctypes.c_byte
-        elif self.__bits_per_pixel == 16:
+        elif self._bits_per_pixel == 16:
             ctype = ctypes.c_uint16
-        elif self.__bits_per_pixel == 32:
+        elif self._bits_per_pixel == 32:
             ctype = ctypes.c_uint32
         else:
             raise RuntimeError("Unsupported bits-per-pixel")
@@ -396,11 +396,11 @@ class Vnc(object):
                 fields.append(("pad%d" % bit, ctype, shift - bit))
             fields.append((name, ctype, bit_count[max_value]))
             bit = shift + bit_count[max_value]
-        if bit != self.__bits_per_pixel:
-            fields.append(("pad%d" % bit, ctype, self.__bits_per_pixel - bit))
-        self.__ctypes_fields = fields
+        if bit != self._bits_per_pixel:
+            fields.append(("pad%d" % bit, ctype, self._bits_per_pixel - bit))
+        self._ctypes_fields = fields
 
-        if self.__big_endian_flag:
-            self.__ctypes_base_class = ctypes.BigEndianStructure
+        if self._big_endian_flag:
+            self._ctypes_base_class = ctypes.BigEndianStructure
         else:
-            self.__ctypes_base_class = ctypes.LittleEndianStructure
+            self._ctypes_base_class = ctypes.LittleEndianStructure
